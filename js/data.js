0aa455a9170f53434802eaaf5eb1a0c249c7ac41
Jennifer Zheng <jinqingmei@jinqingmeideMacBook.local>
2019-10-04 22:03:36 +0800

    My second commit

---
+++ js/data.js
@@ -0,0 +1,83 @@
+var photoCollection = [
+    {
+        id:1,
+        src: "assets/photos/pic1.jpg",
+        where:"uytuyt state",
+        year:2017,
+        month:7,
+        title:"Lost Pedestrain",
+        description:""
+    },
+    {   
+        id:2,
+        src: "assets/photos/pic2.jpg",
+        where:"Deyang",
+        year:2018,
+        month:4,
+        title:"My Birthday date",
+        description:""
+    },
+    {  
+        id:3,
+        src: "assets/photos/pic3.jpg",
+        where:"Minesota",
+        year:2018,
+        month:7,
+        title:"A special corner of Carleton College",
+        description:""
+    },
+    {  
+        id:4,
+        src: "assets/photos/pic4.jpg",
+        where:"Aba State",
+        year:2017,
+        month:7,
+        title:"Prairie Mirror",
+        description:""
+    },
+    {  
+        id:5,
+        src: "assets/photos/pic5.jpg",
+        where:"Minesota",
+        year:2018,
+        month:7,
+        title:"feeble street lamp ",
+        description:""
+    },
+    {  
+        id:6,
+        src: "assets/photos/pic6.jpg",
+        where:"九曲十八弯",
+        year:2016,
+        month:7,
+        title:"Farewell",
+        description:""
+    },
+    {  
+        id:7,
+        src: "assets/photos/pic7.jpg",
+        where:"Deyang",
+        year:2015,
+        month:3,
+        title:"Swan Lake",
+        description:""
+    },
+    {  
+        id:8,
+        src: "assets/photos/pic8.jpg",
+        where:"Deyang",
+        year:2015,
+        month:3,
+        title:"Incoming Autumn",
+        description:""
+    },
+    {  
+        id:9,
+        src: "assets/photos/pic9.jpg",
+        where:"Deyang",
+        year:2018,
+        month:4,
+        title:"Old Fishman",
+        description:""
+    },
+     ]
