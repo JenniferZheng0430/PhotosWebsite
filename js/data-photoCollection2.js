c91b7fd5d3445ff204a13dc3ebe1af693b764492
Jennifer Zheng <jinqingmei@jinqingmeideMacBook.local>
2019-10-14 17:16:18 +0800

    13 commit

--- js/data-photoCollection2.js
+++ js/data-photoCollection2.js
@@ -81,4 +81,139 @@ var photoCollection = [
         title:"trees",
         description:""
     },
+    {  
+        id:10,
+        src: "assets/photos/p10.jpg",
+        where:"Deyang",
+        year:2018,
+        month:4,
+        title:"twisted trees",
+        description:""
+    },
+    {  
+        id:11,
+        src: "assets/photos/p11.jpg",
+        where:"Qingdao",
+        year:2019,
+        month:7,
+        title:"Xinghai Square",
+        description:""
+    },
+    {  
+        id:12,
+        src: "assets/photos/p12.jpg",
+        where:"Qingdao",
+        year:2019,
+        month:7,
+        title:"old house",
+        description:""
+    },
+    {  
+        id:13,
+        src: "assets/photos/p13.jpg",
+        where:"Qingdao",
+        year:2019,
+        month:7,
+        title:"Rainbow on stone tablet",
+        description:""
+    },
+    {  
+        id:15,
+        src: "assets/photos/p15.jpg",
+        where:"Qingdao",
+        year:2019,
+        month:7,
+        title:"light rain",
+        description:""
+    },
+    {  
+        id:16,
+        src: "assets/photos/p16.jpg",
+        where:"Deyang",
+        year:2017,
+        month:3,
+        title:"See sunrise through glass",
+        description:""
+    },
+    {  
+        id:17,
+        src: "assets/photos/p17.jpg",
+        where:"Deyang",
+        year:2016,
+        month:5,
+        title:"Jin River",
+        description:""
+    },
+    {  
+        id:18,
+        src: "assets/photos/p18.jpg",
+        where:"Deyang",
+        year:2015,
+        month:1,
+        title:"The sun is on my finger",
+        description:""
+    },
+    {  
+        id:19,
+        src: "assets/photos/p16.jpg",
+        where:"Jiuzhai",
+        year:2014,
+        month:7,
+        title:"Jiuzhai River",
+        description:""
+    },
+    {  
+        id:20,
+        src: "assets/photos/p20.jpg",
+        where:"Tibet",
+        year:2016,
+        month:7,
+        title:"Above the clouds",
+        description:""
+    },
+    {  
+        id:21,
+        src: "assets/photos/p21.jpg",
+        where:"Tibet",
+        year:2016,
+        month:7,
+        title:"Sunrise",
+        description:""
+    },
+    {  
+        id:22,
+        src: "assets/photos/p22.jpg",
+        where:"Tibet",
+        year:2016,
+        month:7,
+        title:"Sunrise",
+        description:""
+    },
+    {  
+        id:23,
+        src: "assets/photos/p23.jpg",
+        where:"Tibet",
+        year:2016,
+        month:7,
+        title:"Sunrise",
+        description:""
+    },
+    {  
+        id:25,
+        src: "assets/photos/p25.jpg",
+        where:"Tibet",
+        year:2016,
+        month:7,
+        title:"Myself",
+        description:""
+    },
+    {  
+        id:26,
+        src: "assets/photos/p26.jpg",
+        where:"Qingdao",
+        year:2019,
+        month:7,
+        title:"Special design in a restaurant",
+        description:""
+    },
      ]
