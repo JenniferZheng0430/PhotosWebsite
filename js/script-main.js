aac56915b39381fab8a5bcadf4aeefbd202ed282
Jennifer Zheng <jinqingmei@jinqingmeideMacBook.local>
2019-10-15 22:17:54 +0800

    my 19th commit

--- js/script-main.js
+++ js/script-main.js
@@ -1,61 +1,61 @@
 
 
-// const overlay = document.getElementById('overlay-intro');
-// const close = document.getElementById('close-popup');
-// const intro = document.getElementById('popup-intro');
+const overlay = document.getElementById('overlay-intro');
+const close = document.getElementById('close-popup');
+const intro = document.getElementById('popup-intro');
 
 
 
-// const closePopup = () => {
-//     overlay.style.display = "none";
-//     overlay.style.opacity = 0;
-//     intro.style.opacity = 0;
-//     guide()
-// };
+const closePopup = () => {
+    overlay.style.display = "none";
+    overlay.style.opacity = 0;
+    intro.style.opacity = 0;
+    guide()
+};
  
-// const showClose = () => {
-//     close.style.display = "block";
-//     close.style.opacity = 1;
-// };
+const showClose = () => {
+    close.style.display = "block";
+    close.style.opacity = 1;
+};
 
-// const typeWriter = function(txt, id, speed) {
-//     setTimeout(function(){
-//         document.getElementById(id).innerHTML += txt[0]
-//         txt = txt.slice(1, txt.length)
-//         if(txt.length > 0){
-//             typeWriter(txt, id, speed)
-//         }
-//     }, speed);
-// }
+const typeWriter = function(txt, id, speed) {
+    setTimeout(function(){
+        document.getElementById(id).innerHTML += txt[0]
+        txt = txt.slice(1, txt.length)
+        if(txt.length > 0){
+            typeWriter(txt, id, speed)
+        }
+    }, speed);
+}
 
-// const showOverlay = () => {
-//     overlay.classList.add('active')
+const showOverlay = () => {
+    overlay.classList.add('active')
 
-//     $('#popup-text').append($('<h2 id = "popup-text-h" style="text-align: center"></h2>'))
-//     let txt = 'Welcome to my portfolio!'
-//     let id = 'popup-text-h'
-//     let speed = 10
-//     typeWriter(txt, id, speed)
-//     setTimeout(function(){
-//         showClose()
-//         $('#popup-text').append($('<div style="width:100%; height: 1px; background-image: linear-gradient(to right, transparent, #e0e0e0, transparent); margin-bottom: 20px"></div>'))
-//         $('#popup-text').append($('<p id="popup-text-p1"></p>'))
-//         $('#popup-text').append($('<p id="popup-text-p2" style="font-size:20px"></p>'))
-//         $('#popup-text').append($('<p id="popup-text-p3" style="font-size:8px; margin-top:20px; float: right"></p>'))
-//     }, 500)
-//     setTimeout(function(){
-//         txt = 'I coded this website to show apps, games, applied stats poster, and photography website I made. It also includes what I have learned from these processes. I hope you can see more about me from this website!'
-//         id = "popup-text-p1"
-//         speed = 20
-//         typeWriter(txt, id, speed)
-//     }, 2000)
-//     setTimeout(function(){
-//         txt = 'Hit on the X botton to begin!'
-//         id = "popup-text-p3"
-//         speed = 20
-//         typeWriter(txt, id, speed)
-//     }, 6000)
-// };
+    $('#popup-text').append($('<h2 id = "popup-text-h" style="text-align: center"></h2>'))
+    let txt = 'Welcome to my portfolio!'
+    let id = 'popup-text-h'
+    let speed = 10
+    typeWriter(txt, id, speed)
+    setTimeout(function(){
+        showClose()
+        $('#popup-text').append($('<div style="width:100%; height: 1px; background-image: linear-gradient(to right, transparent, #e0e0e0, transparent); margin-bottom: 20px"></div>'))
+        $('#popup-text').append($('<p id="popup-text-p1"></p>'))
+        $('#popup-text').append($('<p id="popup-text-p2" style="font-size:20px"></p>'))
+        $('#popup-text').append($('<p id="popup-text-p3" style="font-size:8px; margin-top:20px; float: right"></p>'))
+    }, 500)
+    setTimeout(function(){
+        txt = 'I coded this website to show apps, games, applied stats poster, and photography website I made. It also includes what I have learned from these processes. I hope you can see more about me from this website!'
+        id = "popup-text-p1"
+        speed = 20
+        typeWriter(txt, id, speed)
+    }, 2000)
+    setTimeout(function(){
+        txt = 'Hit on the X botton to begin!'
+        id = "popup-text-p3"
+        speed = 20
+        typeWriter(txt, id, speed)
+    }, 6000)
+};
  
-// setTimeout(showOverlay, 1000);
+setTimeout(showOverlay, 1000);
 
