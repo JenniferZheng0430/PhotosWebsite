57debbda02ebd99926ed20cc2305e06551236a18
Jennifer Zheng <jinqingmei@jinqingmeideMacBook.local>
2019-10-18 20:44:00 +0800

    my 22th commit: alt change

--- js/script-photoCollection.js
+++ js/script-photoCollection.js
@@ -32,7 +32,7 @@ console.log(data)
           <img src="bgPhoto.jpg">
         </a> */}
 
-var makeImage = '<img class="card-img-bottom" src="'+src+'" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">'
+var makeImage = '<img class="card-img-bottom" src="'+src+'" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Waiting for image loading">'
 var makeLightBox = '<a href="'+src+'" data-lightbox="roadtrip">'+makeImage+'</a>'
 var makeText = '<p class="card-text">"'+title+'"----shot in '+year+'.'+month+'</p>'
 var makeLocation = '<div class="d-flex justify-content-between align-items-center"><small class="text-muted">'+where+'</small></div>'
